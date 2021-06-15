06b2dbd1e6f1b628fd8b6a147d5e4e1685073885
uxl21 <uxl21@nate.com>
2021-06-16 00:02:39 +0900

    1. qrcode library test

--- uxl21pyqrc.py
+++ uxl21pyqrc.py
@@ -7,6 +7,12 @@
     -----
     uxl21
 """
+from platform import version
+from textwrap import fill
+from _pytest._io.wcwidth import wcwidth
+import qrcode
+from qrcode.main import QRCode
+
 
 class QRCodeImageGenerator:
     """
@@ -23,4 +29,28 @@ class QRCodeImageGenerator:
 
 
 if __name__ == "__main__":
-    generator:QRCodeImageGenerator = QRCodeImageGenerator()
+    qrCodeData = """
+        {
+            title: "Naver Blog",
+            url: "https://blog.naver.com/uxl21"
+        }
+    """
+
+    qr = qrcode.QRCode(
+        version=8
+        # box_size=10,
+        # border=4
+    )
+    qr.add_data(qrCodeData)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="#333333", back_color="#ffffff")
+    # img.width = 10
+    # img.pixel_size = 100
+    img.save("/Volumes/OPSLAGPLAATS/WERKTAFEL/qrcode8.png", "png")
+
+    # img.pixel_size = (img.width + img.border * 2) * img.box_size
+    # 490 = (41 + 4 * 2) * 10
+
+    print()
+    print(img.pixel_size, img.width, img.box_size, img.border)
