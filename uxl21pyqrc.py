958885f2f3502cf7e26b594c1eb3b5932cf4e242
Your Name <sklee21@gaubiz.com>
2021-06-15 17:43:19 +0900

    Initial commit

---
+++ uxl21pyqrc.py
@@ -0,0 +1,24 @@
+"""
+    Copyright (c) 2021 uxl21 <uxl21x@gmail.com>
+    This file is a part of uxl21py
+
+    
+    Author
+    -----
+    uxl21
+"""
+
+class QRCodeImageGenerator:
+    """
+        This class generates QR code image.
+
+        Author
+        -----
+        uxl21
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
