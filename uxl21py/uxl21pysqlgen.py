9389f595a7df5eea00c4f42cb967ab57c4f94c0d
uxl21 <uxl21@nate.com>
2022-01-19 18:48:46 +0900

    In development...

--- uxl21py/uxl21pysqlgen.py
+++ uxl21py/uxl21pysqlgen.py
@@ -13,27 +13,60 @@
 import os
 import sys
 from typing import Dict, List
+from uxl21py.uxl21pydata import JSONConfigData
 from uxl21pybatis import PybatisSQLMapper, OracleClientSession
 
 
 class SQLGenerator():
     
-    def __init__(self) -> None:
+    def __init__(self, configPath:str) -> None:
+        """
+            Constructor
+        """
+        
+        self.configData = JSONConfigData(configPath)        
+        
+        
+        #
+        # path info
         basePath = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
-        currPath = os.path.join(basePath, "./uxl21py/resources/mappers/sqlgen_sql.xml")
-        print("currPath =======>>>>>>>  " + currPath)
-    
-        self.sqlMapper = PybatisSQLMapper(currPath)
+        sqlMapperPath = os.path.join(basePath, self.configData.getConfig("sqlmapper_path"))
+        oracleClientPath = self.configData.getConfig("oracle_client_path")
+        
+        newPath = oracleClientPath + ";" + os.getenv("PATH")
+        os.putenv("PATH", newPath)
+        
+        
+        #
+        # database connection info
+        address = self.configData.getConfig("address")
+        port = self.configData.getConfig("port")
+        sid = self.configData.getConfig("sid")
+        dbUrl = address + ":" + port + "/" + sid
+        
+        user = self.configData.getConfig("user")
+        password = self.configData.getConfig("password")
+        
+        
+        #
+        # creates mapper and SQL client
+        self.sqlMapper = PybatisSQLMapper(sqlMapperPath)
         self.sqlClient = OracleClientSession(self.sqlMapper)
-        self.sqlClient.connect(url="218.152.158.146:6837/ORCLCDB", user="SFBO", password="sfbo")
+        self.sqlClient.connect(url=dbUrl, user=user, password=password)
         
         
     def selectUserList(self) -> List[Dict]:
+        """
+            Selects user list of current database.
+        """
+        
         return self.sqlClient.selectList("SQLGEN.selectUserlist")
 
 
+## pyinstaller --onefile --add-data "./uxl21py/resources/mappers/sqlgen_sql.xml;./uxl21py/resources/mappers" ./uxl21py/uxl21pysqlgen.py
 if __name__ == "__main__":
-    ## pyinstaller --onefile --add-data "./uxl21py/resources/mappers/sqlgen_sql.xml;./uxl21py/resources/mappers" ./uxl21py/uxl21pysqlgen.py
+    
+    
     newPath = "C:\\app\\sklee21\\product\\12.2.0\\client_3;" + os.getenv("PATH")
 
     os.putenv("NLS_LANG", "AMERICAN_AMERICA.UTF8")
